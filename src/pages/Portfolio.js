29f0cfe3382818f3c7d8e9f5d4568c4aab2c6464
s-moe <sarahannmoe@gmail.com>
2021-10-06 13:52:22 -0700

    portfolio page changes made

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,77 +2,50 @@ import React, { useState } from 'react';
 
 export default function Portfolio(props) {
 	return (
-		<section className="content-section" id="portfolio">
-			<div className="container px-4 px-lg-5">
-				<div className="content-section-heading text-center">
-					<h3 className="text-secondary mb-0">Portfolio</h3>
-					<h2 className="mb-5">Recent Projects</h2>
-				</div>
-				<div className="row gx-0">
-					<div className="col-lg-6">
-						<a className="portfolio-item" href="#!">
-							<div className="caption">
-								<div className="caption-content">
-									<div className="h2">Dotty</div>
-									<p className="mb-0">
-										A registry for non-traditional celebrations. Built using...
-									</p>
-								</div>
-							</div>
-							<img
-								className="img-fluid"
-								src="/img/DottyImg.png"
-								alt="image of registry application"
-							/>
-						</a>
-					</div>
-					<div className="col-lg-6">
-						<a className="portfolio-item" href="#!">
-							<div className="caption">
-								<div className="caption-content">
-									<div className="h2">Project 4</div>
-									<p className="mb-0">...Built using...</p>
-								</div>
-							</div>
-							<img
-								className="img-fluid"
-								src="assets/img/portfolio-2.jpg"
-								alt="..."
-							/>
-						</a>
-					</div>
-					<div className="col-lg-6">
-						<a className="portfolio-item" href="#!">
-							<div className="caption">
-								<div className="caption-content">
-									<div className="h2">Tool Library</div>
-									<p className="mb-0">...built using...</p>
-								</div>
-							</div>
-							<img
-								className="img-fluid"
-								src="assets/img/portfolio-3.jpg"
-								alt="..."
-							/>
-						</a>
-					</div>
-					<div className="col-lg-6">
-						<a className="portfolio-item" href="#!">
-							<div className="caption">
-								<div className="caption-content">
-									<div className="h2">Bookmarks</div>
-									<p className="mb-0">...built using</p>
-								</div>
-							</div>
-							<img
-								className="img-fluid"
-								src="assets/img/portfolio-4.jpg"
-								alt="..."
-							/>
-						</a>
-					</div>
-				</div>
+		<div className="container">
+			<div className="grid-1 grow">
+				<a
+					className="portfolio-item"
+					href="https://sarah-moe-project3.herokuapp.com/"
+					target="_blank"
+				>
+					<img
+						className="img-fluid"
+						src="/img/DottyImg.png"
+						alt="image of registry application"
+					/>
+				</a>
 			</div>
-		</section>
+
+			<div className="grid-2">
+				<a className="portfolio-item" href="#!">
+					<img
+						className="img-fluid"
+						src="assets/img/portfolio-2.jpg"
+						alt="..."
+					/>
+				</a>
+			</div>
+
+			<div className="grid-3">
+				<a className="portfolio-item" href="#!">
+					<img
+						className="img-fluid"
+						src="assets/img/portfolio-3.jpg"
+						alt="..."
+					/>
+				</a>
+			</div>
+
+			<div className="grid-4">
+				<a className="portfolio-item" href="#!">
+					<img
+						className="img-fluid"
+						src="assets/img/portfolio-4.jpg"
+						alt="..."
+					/>
+				</a>
+			</div>
+		</div>
 	);
 }
