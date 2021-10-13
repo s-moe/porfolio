bd4711d1ec3ba2f6fc394b91ca480ebbf0942371
s-moe <sarahannmoe@gmail.com>
2021-10-13 07:20:24 -0700

    thank you page styled

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ThankYou() {
 	return (
 		<div className="thank-you-msg">
-			<h1>Thank you for your email! </h1>
+			<h1 className="h1">Thank you for your email! </h1>
 			<p>I'll be in touch soon.</p>
 		</div>
 	);
